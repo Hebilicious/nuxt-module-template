672bb07340eebd0ec5681cedca80ba7350e95c7e
Hebilicious <xsh4k3@gmail.com>
2023-06-04 22:17:21 +0700

    ci: fix test setup

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -2,12 +2,12 @@ import { fileURLToPath } from "node:url"
 import { describe, expect, it } from "vitest"
 import { $fetch, setup } from "@nuxt/test-utils"
 
-describe("vue-query", async () => {
+describe("basic test", async () => {
   await setup({
-    rootDir: fileURLToPath(new URL("./fixtures/todos", import.meta.url))
+    rootDir: fileURLToPath(new URL("./fixtures/basic", import.meta.url))
   })
 
-  it("renders the todos", async () => {
+  it("displays data", async () => {
     // Get response to a server-rendered page with `$fetch`.
     const html = await $fetch("/")
     expect(html).toContain("Hello")
